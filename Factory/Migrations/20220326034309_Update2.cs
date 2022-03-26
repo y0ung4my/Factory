124a4a209c5808d1fed028c20b24832cf0ff8be4
Amy Young <youngamy1223@gmail.com>
2022-03-25 22:19:30 -0600

    update FactoryContext and Migrate

---
+++ Factory/Migrations/20220326034309_Update2.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Update2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EngineerMachine_Machine_MachineId",
+                table: "EngineerMachine");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Machine",
+                table: "Machine");
+
+            migrationBuilder.RenameTable(
+                name: "Machine",
+                newName: "Machines");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Machines",
+                table: "Machines",
+                column: "MachineId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EngineerMachine_Machines_MachineId",
+                table: "EngineerMachine",
+                column: "MachineId",
+                principalTable: "Machines",
+                principalColumn: "MachineId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EngineerMachine_Machines_MachineId",
+                table: "EngineerMachine");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Machines",
+                table: "Machines");
+
+            migrationBuilder.RenameTable(
+                name: "Machines",
+                newName: "Machine");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Machine",
+                table: "Machine",
+                column: "MachineId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EngineerMachine_Machine_MachineId",
+                table: "EngineerMachine",
+                column: "MachineId",
+                principalTable: "Machine",
+                principalColumn: "MachineId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -5,7 +5,7 @@ namespace Factory.Models
   public class FactoryContext : DbContext
   {
     public DbSet<Engineer> Engineers { get; set; }
-    public DbSet<Machine> Machine { get; set; }
+    public DbSet<Machine> Machines { get; set; }
     public DbSet<EngineerMachine> EngineerMachine { get; set; }
     public FactoryContext(DbContextOptions options) : base(options) { }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
